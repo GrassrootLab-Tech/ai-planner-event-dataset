42a5e14dbc7c836c5546cceacc5ffce246ec1e56
Anurag Tomar <anurag@grassrootlab.com>
2026-08-04 18:44:36 +0530

    some urls

--- vendor_profiles/sample_urls.py
+++ vendor_profiles/sample_urls.py
@@ -1,68 +1,68 @@
 PAGE_URLS: list[dict[str, str | None]] = [
-    # {
-    #     "page_url": "https://www.thebash.com/dj/dj-guitarist-bagpiper-michael-lancaster",
-    #     "page_title": None,
-    # },
-    # {
-    #     "page_url": "https://www.thebash.com/photographer/crystaline-photography",
-    #     "page_title": None,
-    # # },
-    # {
-    #     "page_url": "https://www.gigsalad.com/deja_vu_productions",
-    #     "page_title": None,
-    # },
-    # {
-    #     "page_url": "https://www.gigsalad.com/elena_abshire_dba_great_tango_conjunction_denver",
-    #     "page_title": None,
-    # },
-    # {
-    #     "page_url": "https://www.theknot.com/marketplace/dj-connection-denver-co-951821",
-    #     "page_title": None,
-    # },
-    # {
-    #     "page_url": "https://www.theknot.com/marketplace/have-and-to-hold-photography-greenwood-village-co-2018206",
-    #     "page_title": None,
-    # },
-    # {
-    #     "page_url": "https://www.partyslate.com/vendors/sarah-roshan-collective",
-    #     "page_title": None,
-    # },
-    # {
-    #     "page_url": "https://www.partyslate.com/vendors/catering-by-design",
-    #     "page_title": None,
-    # },
-    # {
-    #     "page_url": "https://www.zola.com/wedding-vendors/wedding-bands-djs/signature-celebrations",
-    #     "page_title": None,
-    # },
-    # {
-    #     "page_url": "https://www.zola.com/wedding-vendors/wedding-catering/brothers-bbq",
-    #     "page_title": None,
-    # },
-    # {
-    #     "page_url": "https://www.weddingwire.com/biz/abc-hair-and-makeup/da5802aee2d4762b.html",
-    #     "page_title": None,
-    # },
-    # {
-    #     "page_url": "https://www.weddingwire.com/biz/good-to-go-djs-and-entertainment/4bcceeaa8d0506ca.html",
-    #     "page_title": None,
-    # },
-    # {
-    #     "page_url": "https://www.thumbtack.com/co/denver/magicians/comedy-magician-children-adult-entertainment/service/146631760033621185",
-    #     "page_title": None,
-    # },
-    # {
-    #     "page_url": "https://www.thumbtack.com/co/erie/personal-chefs/private-chef-events/service/559762247329136668",
-    #     "page_title": None,
-    # },
-    # {
-    #     "page_url": "https://www.eventective.com/denver-co/professional-video-and-photography-photographer-558056.html",
-    #     "page_title": None,
-    # },
-    # {
-    #     "page_url": "https://www.eventective.com/366899/Magician-Dewayne-Hill.html",
-    #     "page_title": None,
-    # },
+    {
+        "page_url": "https://www.thebash.com/dj/dj-guitarist-bagpiper-michael-lancaster",
+        "page_title": None,
+    },
+    {
+        "page_url": "https://www.thebash.com/photographer/crystaline-photography",
+        "page_title": None,
+    },
+    {
+        "page_url": "https://www.gigsalad.com/deja_vu_productions",
+        "page_title": None,
+    },
+    {
+        "page_url": "https://www.gigsalad.com/elena_abshire_dba_great_tango_conjunction_denver",
+        "page_title": None,
+    },
+    {
+        "page_url": "https://www.theknot.com/marketplace/dj-connection-denver-co-951821",
+        "page_title": None,
+    },
+    {
+        "page_url": "https://www.theknot.com/marketplace/have-and-to-hold-photography-greenwood-village-co-2018206",
+        "page_title": None,
+    },
+    {
+        "page_url": "https://www.partyslate.com/vendors/sarah-roshan-collective",
+        "page_title": None,
+    },
+    {
+        "page_url": "https://www.partyslate.com/vendors/catering-by-design",
+        "page_title": None,
+    },
+    {
+        "page_url": "https://www.zola.com/wedding-vendors/wedding-bands-djs/signature-celebrations",
+        "page_title": None,
+    },
+    {
+        "page_url": "https://www.zola.com/wedding-vendors/wedding-catering/brothers-bbq",
+        "page_title": None,
+    },
+    {
+        "page_url": "https://www.weddingwire.com/biz/abc-hair-and-makeup/da5802aee2d4762b.html",
+        "page_title": None,
+    },
+    {
+        "page_url": "https://www.weddingwire.com/biz/good-to-go-djs-and-entertainment/4bcceeaa8d0506ca.html",
+        "page_title": None,
+    },
+    {
+        "page_url": "https://www.thumbtack.com/co/denver/magicians/comedy-magician-children-adult-entertainment/service/146631760033621185",
+        "page_title": None,
+    },
+    {
+        "page_url": "https://www.thumbtack.com/co/erie/personal-chefs/private-chef-events/service/559762247329136668",
+        "page_title": None,
+    },
+    {
+        "page_url": "https://www.eventective.com/denver-co/professional-video-and-photography-photographer-558056.html",
+        "page_title": None,
+    },
+    {
+        "page_url": "https://www.eventective.com/366899/Magician-Dewayne-Hill.html",
+        "page_title": None,
+    },
     {
         "page_url": "https://www.thebash.com/search/blues-band-denver-co",
         "page_title": None,
